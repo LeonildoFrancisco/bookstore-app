c026cb2d59f627710df4974ba6f88ff7c867127d
LeonildoFrancisco <leonildojafrancisco@gmail.com>
2021-07-10 17:21:13 +0100

    Ultimos ajustes

--- src/main/java/com/leonildo/bookstore/resource/LivroResource.java
+++ src/main/java/com/leonildo/bookstore/resource/LivroResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.leonildo.bookstore.domain.Livro;
 import com.leonildo.bookstore.dtos.LivroTDO;
 import com.leonildo.bookstore.service.LivroService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/livros")
 public class LivroResource {
